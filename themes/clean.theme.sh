5364290482f69d8bab81c14acd494ef7eea81da1
Jacob Dick <jacobkdick@gmail.com>
2018-09-13 09:29:24 -0600

    feat(themes) add jacob theme

--- themes/clean.theme.sh
+++ themes/clean.theme.sh
@@ -1,7 +1,3 @@
-# git theming
-SCM_THEME_PROMPT_PREFIX="${bold_blue}(${yellow}"
-SCM_THEME_PROMPT_SUFFIX="${bold_blue})${reset_color} "
-SCM_THEME_PROMPT_CLEAN=""
-SCM_THEME_PROMPT_DIRTY="${bold_red}✗"
-
 PS1="\[$text_reset\]\u:\[$text_blue\]\W \[$text_reset\]$ "
+
+export PS1
---
+++ themes/jacob.theme.sh
@@ -0,0 +1,4 @@
+PS1="\[$text_yellow\]\w\[$text_reset\] \$git_branch\$git_untracked\$git_dirty\n"
+PS1+="\[$text_white\]\u@\h \A\[$text_reset\] \$ "
+
+export PS1
