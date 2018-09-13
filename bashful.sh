9bae9562c746b1fe519db5deacffe612ea1ebfac
Jacob Dick <jacobkdick@gmail.com>
2018-09-13 14:01:15 -0600

    feat(install) change dir reference for dev installer

--- bashful.sh
+++ bashful.sh
@@ -3,4 +3,4 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BASHFUL_DIR=${BASHFUL_DIR:="$HOME/.bashful"}
 
 # Run the install process.
-source $BASHFUL_DIR/tools/install.sh
+source $CURRENT_DIR/tools/install.sh
