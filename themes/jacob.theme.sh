93eaf633dd3e1a39906c5d8a87fdf58da409fbda
Jacob Dick <jacobkdick@gmail.com>
2018-09-13 09:56:14 -0600

    feat(themes) removed sourcing of base theme from jacob

--- themes/jacob.theme.sh
+++ themes/jacob.theme.sh
@@ -1,5 +1,3 @@
-. $BASHFUL_DIR/themes/_base.theme.sh
-
 PS1="\[$text_yellow\]\w\[$text_reset\] \$(find_git_branch)\$(find_git_untracked)\$(find_git_dirty)\n"
 PS1+="\[$text_white\]\u@\h \A\[$text_reset\] \$ "
 
