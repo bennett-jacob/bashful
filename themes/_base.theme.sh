6e8d69849284741622754e8831f27f6a92e39086
Jacob Dick <jacobkdick@gmail.com>
2018-09-13 09:54:30 -0600

    fix(themes) add git functionality to jacob theme

--- themes/_base.theme.sh
+++ themes/_base.theme.sh
@@ -1,6 +1,5 @@
 ####################
 # Git
-
 function find_git_branch {
     # Based on: http://stackoverflow.com/a/13003854/170413
     local branch
@@ -8,9 +7,9 @@ function find_git_branch {
         if [[ "$branch" == "HEAD" ]]; then
             branch='detached*'
         fi
-        git_branch="$text_purple ✪ $branch$text_reset"
+        echo "$text_purple ✪ $branch$text_reset"
     else
-        git_branch=""
+        return
     fi
 }
 
@@ -31,9 +30,9 @@ function find_git_untracked {
     local num_untracked=$(git status --porcelain 2>/dev/null | grep "^??" | wc -l)
     if [[ $num_untracked -gt 0 ]]; then
         clean_num_untracked=$(echo $num_untracked | grep -o '[0-9]*')
-        git_untracked="$text_red ⚠︎ $clean_num_untracked$text_reset"
+        echo "$text_red ⚠︎ $clean_num_untracked$text_reset"
     else
-        git_untracked=""
+        return
     fi
 }
 
@@ -41,12 +40,8 @@ function find_git_dirty {
     local num_dirty=$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)
     if [[ $num_dirty -gt 0 ]]; then
         clean_num_dirty=$(echo $num_dirty | grep -o '[0-9]*')
-        git_dirty="$text_green ⌦ $clean_num_dirty$text_reset"
+        echo "$text_green ⌦ $clean_num_dirty$text_reset"
     else
-        git_dirty=""
+        return
     fi
 }
-
-find_git_branch
-find_git_untracked
-find_git_dirty
