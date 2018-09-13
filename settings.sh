31b78883b3f8f00dafaf98d359ee01d2e2f9f245
Jacob Dick <jacobkdick@gmail.com>
2018-09-13 14:16:15 -0600

    chore(settings) remove unused settings file

--- settings.sh
+++
@@ -1,8 +0,0 @@
-# These settings are available from any file sourced by ./bashful.sh
-
-export BASHFUL_AUTOUPDATE=true
-
-export BASHFUL_TARGET_DIR=$HOME/.bashful
-
-# Colors
-source $BASHFUL_DIR/modules/colors.sh
