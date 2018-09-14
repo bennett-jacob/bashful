301644867dd72e88bf9a84c83dd3eb0e02da0320
Jacob Dick <jacobkdick@gmail.com>
2018-09-14 13:34:06 -0600

    fix(themes) add check for .git dir for all functions

--- themes/_base.theme.sh
+++ themes/_base.theme.sh
@@ -1,15 +1,15 @@
 ####################
 # Git
 function find_git_branch {
-    # Based on: http://stackoverflow.com/a/13003854/170413
-    local branch
-    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-        if [[ "$branch" == "HEAD" ]]; then
-            branch='detached*'
+    if [[ -d .git ]]; then
+        # Based on: http://stackoverflow.com/a/13003854/170413
+        local branch
+        if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+            if [[ "$branch" == "HEAD" ]]; then
+                branch='detached*'
+            fi
+            echo "$text_purple ✪ $branch$text_reset"
         fi
-        echo "$text_purple ✪ $branch$text_reset"
-    else
-        :
     fi
 }
 
@@ -27,32 +27,32 @@ function find_git_branch {
 # https://codedump.io/share/JUEEsHxuENuf/1/checking-for-a-dirty-index-or-untracked-files-with-git
 
 function find_git_untracked {
-    local num_untracked=$(git status --porcelain 2>/dev/null | grep "^??" | wc -l)
-    if [[ $num_untracked -gt 0 ]]; then
-        clean_num_untracked=$(echo $num_untracked | grep -o '[0-9]*')
-        echo "$text_red ⚠︎ $clean_num_untracked$text_reset"
-    else
-        :
+    if [[ -d .git ]]; then
+        local num_untracked=$(git status --porcelain 2>/dev/null | grep "^??" | wc -l)
+        if [[ $num_untracked -gt 0 ]]; then
+            clean_num_untracked=$(echo $num_untracked | grep -o '[0-9]*')
+            echo "$text_red ⚠︎ $clean_num_untracked$text_reset"
+        fi
     fi
 }
 
 function find_git_dirty {
-    local num_dirty=$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)
-    if [[ $num_dirty -gt 0 ]]; then
-        clean_num_dirty=$(echo $num_dirty | grep -o '[0-9]*')
-        echo "$text_green ⌦ $clean_num_dirty$text_reset"
-    else
-        :
+    if [[ -d .git ]]; then
+        local num_dirty=$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)
+        if [[ $num_dirty -gt 0 ]]; then
+            clean_num_dirty=$(echo $num_dirty | grep -o '[0-9]*')
+            echo "$text_green ⌦ $clean_num_dirty$text_reset"
+        fi
     fi
 }
 
 function find_git_cached {
-    local num_cached=$(git diff --cached --numstat | wc -l)
-    if [[ $num_cached -gt 0 ]]; then
-        clean_num_cached=$(echo $num_cached | grep -o '[0-9]*')
-        echo "$text_yellow ● $clean_num_cached$text_reset"
-    else
-        :
+    if [[ -d .git ]]; then
+        local num_cached=$(git diff --cached --numstat | wc -l)
+        if [[ $num_cached -gt 0 ]]; then
+            clean_num_cached=$(echo $num_cached | grep -o '[0-9]*')
+            echo "$text_yellow ● $clean_num_cached$text_reset"
+        fi
     fi
 }
 
