0ec355a5eedd55b70e773869c6e9747d5bcf50e2
Jacob Dick <jacobkdick@gmail.com>
2018-09-14 13:25:14 -0600

    feat(themes) add ahead/behind git tracking

--- themes/jacob.theme.sh
+++ themes/jacob.theme.sh
@@ -1,4 +1,4 @@
-PS1="\[$text_yellow\]\w\[$text_reset\] \$(find_git_branch)\$(find_git_untracked)\$(find_git_dirty)\$(find_git_cached)\n"
+PS1="\[$text_yellow\]\w\[$text_reset\] \$(find_git_branch)\$(find_behind_ahead)\$(find_git_untracked)\$(find_git_dirty)\$(find_git_cached)\n"
 PS1+="\[$text_white\]\u@\h \A\[$text_reset\] \$ "
 
 export PS1
