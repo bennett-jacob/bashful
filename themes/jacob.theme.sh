a77a22b94e549d6e6998a3094eb7290561a88413
Jacob Dick <jacobkdick@gmail.com>
2018-09-30 18:52:06 -0600

    fix(themes) fix broken git status

--- themes/jacob.theme.sh
+++ themes/jacob.theme.sh
@@ -1,6 +1,6 @@
 
 
-PS1="\[$text_yellow\]\w\[$text_reset\] \$($text_purple$git_branch$text_red ⚠︎ $git_untracked)\n"
+PS1="\[$text_yellow\]\w\[$text_reset\] \[$text_purple$git_branch$text_red ⚠︎ $git_untracked\]\n"
 PS1+="\[$text_white\]\u@\h \A\[$text_reset\] \$ "
 
 export PS1
