fcf0720b4e35fa2a42a2b106b1df676364da656f
Jacob Dick <jacobkdick@gmail.com>
2018-09-30 19:23:37 -0600

    fix(themes) add more brackets

--- themes/jacob.theme.sh
+++ themes/jacob.theme.sh
@@ -4,15 +4,15 @@ function git_ps1 {
     if [ "$is_git_dir" = true ]; then
         printf "$text_purple $git_branch_symbol $git_branch$text_reset"
 
-        if [ $git_updated -gt 0 ]; then
+        if [[ $git_updated -gt 0 ]]; then
             printf "$text_green $git_updated_symbol $git_updated$text_reset"
         fi
 
-        if [ $git_added -gt 0 ]; then
+        if [[ $git_added -gt 0 ]]; then
             printf "$text_green $git_added_symbol $git_added$text_reset"
         fi
 
-        if [ $git_untracked -gt 0 ]; then
+        if [[ $git_untracked -gt 0 ]]; then
             printf "$text_red $git_untracked_symbol $git_untracked$text_reset"
         fi
     fi
