008b521651d502f37a1b354590f1c4e9813ff069
Jacob Dick <jacobkdick@gmail.com>
2018-09-30 18:50:30 -0600

    feat(themes) add better git status

--- themes/_base.theme.sh
+++ themes/_base.theme.sh
@@ -1,5 +1,45 @@
 ####################
 # Git
+function git_status {
+    if [[ -d .git ]]; then
+        # https://codereview.stackexchange.com/a/117675/
+        git status --porcelain -b | (
+            unset git_branch git_updated git_added git_deleted git_untracked git_ahead git_behind git_renamed
+            git_updated=0
+            git_added=0
+            git_deleted=0
+            git_untracked=0
+            git_ahead=0
+            git_behind=0
+            git_renamed=0
+            while read line ; do
+                case "$line" in
+                    "## "*)
+                        # Get the local branch name
+                        git_branch=$(echo "$line" | cut -d " " -f2 | cut -d. -f1)
+                        # TODO: get commits ahead/behind
+                        ;;
+                    @[:space:M]|[MM]|[M:space:]*)
+                        git_updated=$((git_updated + 1))
+                        ;;
+                    @[:space:A]|[AA]|[A:space:]*)
+                        git_added=$((git_added + 1))
+                        ;;
+                    @[DD]|[D:space:]*)
+                        git_deleted=$((git_deleted + 1))
+                        ;;
+                    @[:space:?]|[??]|[?:space:]*)
+                        git_untracked=$((git_untracked + 1))
+                        ;;
+                    # @[:space:D]*)
+                    #     git_renamed=$((git_renamed + 1))
+                    #     ;;
+                esac
+            done
+        )
+    fi
+}
+
 function git_info {
     if [[ -d .git ]]; then
         # Based on: http://stackoverflow.com/a/13003854/170413
