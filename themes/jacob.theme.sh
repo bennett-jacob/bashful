0faf199ab734cea4e1a73696e74654498c8a4402
Jacob Dick <jacobkdick@gmail.com>
2018-09-30 19:17:51 -0600

    fix(themes) add execution for function

--- themes/jacob.theme.sh
+++ themes/jacob.theme.sh
@@ -2,13 +2,14 @@
 
 function git_ps1 {
     git_status
-
+    echo "$is_git_dir"
+    echo "one"
     if [ $is_git_dir ]; then
         printf "$text_purple $git_branch_symbol $git_branch$text_reset"
     fi
 }
 
-PS1="\[$text_yellow\]\w\[$text_reset\] $(git_ps1)\n"
+PS1="\[$text_yellow\]\w\[$text_reset\] \$(git_ps1)\n"
 PS1+="\[$text_white\]\u@\h \A\[$text_reset\] \$ "
 
 export PS1
