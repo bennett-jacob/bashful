9e9f9b3cd0d49df57e6be57e4cb77609fc36816d
Jacob Dick <jacobkdick@gmail.com>
2018-10-25 12:25:11 -0600

    fix help text echo

--- plugins/plugins.sh
+++ plugins/plugins.sh
@@ -1,16 +1,16 @@
 _plugin_help_text() {
-    default_plugin_help_text = "Plugins are loaded in via .bashrc"
+    DEFAULT_PLUGIN_HELP_TEXT="Plugins are loaded in via .bashrc"
     if [ -z "$1" ] # is parameter #1 zero length?
     then
         # no parameter passed.
-        echo "$default_plugin_help_text"
+        echo $DEFAULT_PLUGIN_HELP_TEXT
     else
         if [ -f $BASHFUL_DIR/plugins/$1/help ]; then
             # if help file exists, print it
             cat $BASHFUL_DIR/plugins/$1/help
         else
             # if no help file, print helpful line
-            echo "$defult_plugin_help_text"
+            echo $DEFAULT_PLUGIN_HELP_TEXT
         fi
     fi
 }
