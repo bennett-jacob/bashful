9ad4f259a862c4b5a79829d53844fa11b156a341
Jacob Dick <jacobkdick@gmail.com>
2018-10-25 12:31:29 -0600

    add line spacing to plugin help text

--- plugins/plugins.sh
+++ plugins/plugins.sh
@@ -1,8 +1,8 @@
 _plugin_help_text() {
     DEFAULT_PLUGIN_HELP_TEXT="Plugins are loaded in via .bashrc"
 
-    printf "$text_yellow"
-    printf "BASHFUL PLUGIN$text_reset\n"
+    printf "\n$text_yellow"
+    printf "BASHFUL PLUGIN API"
 
     if [ -z "$1" ] # is parameter #1 zero length?
     then
@@ -11,12 +11,15 @@ _plugin_help_text() {
     else
         if [ -f $BASHFUL_DIR/plugins/$1/help ]; then
             # if help file exists, print it
+            printf ": $1$text_reset\n"
             cat $BASHFUL_DIR/plugins/$1/help
         else
             # if no help file, print helpful line
             echo $DEFAULT_PLUGIN_HELP_TEXT
         fi
     fi
+
+    printf "\n"
 }
 
 alias plugins="_plugin_help_text"
